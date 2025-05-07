f73c6e35ae093f4b5e6de33ef20b56eee2c3ac11
Pia Rojas <piavalentina.rojas@alumnos.ulagos.cl>
2025-05-07 16:36:11 -0400

    first commit

---
+++ 01-variables.py
@@ -0,0 +1,19 @@
+"""GUIA INICIAL DE PYTHON
+DE VARIABLES - ULAGOS 2025"""
+
+nombre = "Pia" 
+apellido = "Rojas"
+
+print("Hola mi nombre es ", nombre, "y mi apellido es ", apellido)
+
+
+#imprimiendo con operador de concatenación
+print("Hola mi nombre es " + nombre, "y mi apellido es " + apellido)
+
+#imprimiendo con cadenas f-string
+
+print (f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
+
+ciudad, region, pais = "Castro", "Los Lagos", "Chile"
+
+print (f"Hola soy de {ciudad}, {region}, {pais}")
